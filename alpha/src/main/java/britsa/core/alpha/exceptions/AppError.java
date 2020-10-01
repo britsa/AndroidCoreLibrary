5c7cb7a7bd9982cd44a74a88c9b9d9b002339798
Regilan <mariairegilan@gmail.com>
2020-10-02 00:00:42 +0530

    added 11005 AppError value

--- alpha/src/main/java/britsa/core/alpha/exceptions/AppError.java
+++ alpha/src/main/java/britsa/core/alpha/exceptions/AppError.java
@@ -5,7 +5,8 @@ public enum AppError {
     ERROR_SESSION_OBJ_JSON_PROCESSING(11001, "Error when converting object to JSON in session"),
     ERROR_OBJ_JSON_PROCESSING(11002, "Error when converting object to JSON"),
     UNDEFINED_METHOD_TYPE(11003, "Request method type should be one of GET, POST, PUT, PATCH, DELETE, OPTIONS"),
-    SERVICE_CALL_FAILED(11004, "Service Call failed")
+    SERVICE_CALL_FAILED(11004, "Service Call failed"),
+    FIREBASE_REALTIME_DB_READ_ERROR(11005, "Some failure occurred while reading data from Firebase Realtime Database.")
     ;
 
     private int appErrorCode;
